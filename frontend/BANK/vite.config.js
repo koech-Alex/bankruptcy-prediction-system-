bd05cbe23c28197b2559fa151cc44e78b5a2caf6
koech-Alex <koechalex142@gmail.com>
2025-05-03 15:19:20 +0300

    modified

--- frontend/BANK/vite.config.js
+++ frontend/BANK/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: "127.0.0.1",
-    port: 3001, // <-- changed to 3001
+    port: 8000, // <-- changed to 3001
   },
 });
 // https://vitejs.dev/config/
